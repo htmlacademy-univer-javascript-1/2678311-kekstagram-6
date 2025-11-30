d039f71dd37b05f67308bb84955e01e85bd03380
Keks <keks@htmlacademy.ru>
2025-11-30 15:44:19 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,22 @@
+function checkStrLength(inputStr, count) {
+  return inputStr.length <= count;
+}
+
+function isPalindrome(input) {
+  const inputWithoutSpaces = input.replaceAll(' ', '').toLowerCase();
+
+  const length = inputWithoutSpaces.length;
+  for (let i = 0; i < Math.round(length / 2); i++) {
+    if (inputWithoutSpaces[i] !== inputWithoutSpaces[length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const notNumericRegexp = RegExp('\\D', 'gm');
+function parseNumber(input) {
+  const onlyDigits = input.toString().replaceAll(notNumericRegexp, '');
+
+  return parseInt(onlyDigits, 10);
+}
